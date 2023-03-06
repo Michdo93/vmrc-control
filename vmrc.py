3b986570bb135c908ac6a731547d8f813c72d4d2
Michdo93 <michaeldoerflinger93@gmail.com>
2023-03-06 13:39:26 +0100

    Update vmrc.py

--- vmrc.py
+++ vmrc.py
@@ -9,21 +9,41 @@ import multiprocessing
 keyboard = Controller()
 
 def beamer_shell():
-    debug_info = subprocess.call("/usr/bin/vmrc -H "<host>" -U "<username>" -M "<moid>" -X, shell=False)
+    debug_info = subprocess.run('/usr/bin/vmrc -H "<host>" -U "<username>" -M "<moid>" -X', shell=True)
     print(debug_info)
 
 def vmrc_input():
     time.sleep(6)
 
-    pyautogui.click(x=320, y=295, clicks=1, button='left')
+    #pyautogui.click(x=320, y=295, clicks=1, button='left')
+    password_input = pyautogui.locateOnScreen('/home/<user>/Pictures/vmrcpassword.png')
 
-    time.sleep(0.5)
+    if password_input is not None:
+        password_input_center = pyautogui.center(password_input)
+        pyautogui.click(password_input_center)
+    else:
+        return 0
 
-    keyboard.type("<password>")
+    debug = subprocess.run('setxkbmap de', shell=True)
+    print(debug.returncode)
 
     time.sleep(2)
 
-    pyautogui.click(x=475, y=340, clicks=1, button='left')
+    if debug.returncode == 0:
+        keyboard.type("<password>")
+    else:
+        return 0
+
+    time.sleep(3)
+
+    #pyautogui.click(x=475, y=340, clicks=1, button='left')
+    connect_button = pyautogui.locateOnScreen('/home/<user>/Pictures/vmrcconnect.png')
+
+    if connect_button is not None:
+        connect_button_center = pyautogui.center(connect_button)
+        pyautogui.click(connect_button_center)
+    else:
+        pyautogui.click(x=475, y=340, clicks=1, button='left')
 
 
 if __name__ == "__main__":
